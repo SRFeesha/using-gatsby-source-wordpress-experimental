b5b11bf5c1090902faa5e8b163d405f495cb95da
Beniamino Marini <beniaminomarini@Beniaminos-MBP.lan>
2020-10-21 09:11:50 +0200

    Added fetch url

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+          `https://staging-website.wonderflow.co/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
